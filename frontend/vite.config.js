cb9b5cb6f4a9e94d869687192c82fa799ee2eca7
TreezCode <TreezCode@gmail.com>
2023-03-22 20:06:24 -0400

    refactored some contact validation and added backend structure

---
+++ frontend/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000
+    },
+    define: {
+      'process.env': env,
+    },
+  };
+});
