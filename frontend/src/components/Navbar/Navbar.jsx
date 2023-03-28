e0ce9a0dbb90a566e627e36263796f3e591456db
TreezCode <TreezCode@gmail.com>
2023-03-28 19:09:01 -0400

    feat: added blur to navbar and cleaned up some of navbar code

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,18 +12,20 @@ import { logo } from '../../common/assets';
 import './Navbar.css';
 
 const Navbar = () => {
-  const [activeLink, setActiveLink] = useState('');
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState(null);
+  const [menuActive, setMenuActive] = useState(false);
+
+  const navRef = useRef(null);
   const menuRef = useRef(null);
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    setMenuActive(!menuActive);
     const menuElement = menuRef.current;
-    menuElement.classList.toggle('menu-open');
+    menuElement.classList.toggle('menu-active');
 
     // Disable scrolling when menu is open
     const bodyElement = document.querySelector('body');
-    if (menuOpen) {
+    if (menuActive) {
       bodyElement.style.overflow = 'auto';
     } else {
       bodyElement.style.overflow = 'hidden';
@@ -33,7 +35,7 @@ const Navbar = () => {
   const handleLogoClick = () => {
     setActiveLink('');
     window.scrollTo(0, 0);
-    menuOpen && toggleMenu();
+    menuActive && toggleMenu();
   };
 
   const handleMenuClick = (link) => {
@@ -43,19 +45,19 @@ const Navbar = () => {
 
   const handleNavbarClick = (link) => {
     setActiveLink(link.title);
-    menuOpen && toggleMenu();
+    menuActive && toggleMenu();
   };
 
   useEffect(() => {
     const handleScroll = () => {
-      const navbar = document.getElementById('navbar');
+      const navElement = navRef.current;
       const offset = 50;
       const scrollPos = window.scrollY;
 
       if (scrollPos > offset) {
-        navbar.classList.add('navbar-active');
+        navElement.classList.add('navbar-active');
       } else {
-        navbar.classList.remove('navbar-active');
+        navElement.classList.remove('navbar-active');
       }
     };
 
@@ -67,14 +69,61 @@ const Navbar = () => {
 
   return (
     <header>
-      <nav
-        id='navbar'
-        className='navbar fixed top-0 left-0 w-full p-6 z-50 text-white overflow-y-hidden'
-        style={{ transition: '0.7s' }}
-      >
+      <nav className='fixed top-0 left-0 w-full z-50 text-white overflow-y-hidden'>
+        <div ref={navRef} className='nav-container relative w-full z-50 p-6'>
+          <div className='nav-content max-w-[1350px] relative grid justify-between items-center my-0 mx-auto'>
+            <Link
+              to={'/'}
+              className='brand text-[1.6rem] text-[#915eff]'
+              onClick={handleLogoClick}
+            >
+              TreezCode
+            </Link>
+            <div className='nav-container-inner my-0 mx-auto w-full max-w-[1000px] flex justify-between items-center'>
+              <ul className='navlinks w-full my-0 mx-2'>
+                {navLinks.map((link) => (
+                  <li
+                    key={link.id}
+                    className={`${
+                      activeLink === link.title ? 'scale-110' : ''
+                    } text-[18px] font-medium cursor-pointer inline-block px-2 `}
+                    onClick={() => handleNavbarClick(link)}
+                  >
+                    <a href={`#${link.id}`} className='hover:text-secondary'>{link.title}</a>
+                  </li>
+                ))}
+              </ul>
+              <form>
+                <div className='input-wrap flex justify-center items-center py-2'>
+                  <input type='search' placeholder='Search...' className='px-1' />
+                  <button type='submit'>
+                    <FaSearch className='transition duration-300 hover:text-secondary' />
+                  </button>
+                </div>
+              </form>
+            </div>
+
+            {!menuActive ? (
+              <FaBars
+                id='menu-btn'
+                className='text-[1.2rem] cursor-pointer transition duration-300 hover:text-secondary ml-auto'
+                onClick={toggleMenu}
+              />
+            ) : (
+              <FaWindowClose
+                id='menu-btn'
+                className='text-[1.2rem] cursor-pointer transition duration-300 hover:text-secondary ml-auto'
+                onClick={toggleMenu}
+              />
+            )}
+          </div>
+
+
+          </div>
+
         <div
           ref={menuRef}
-          className='menu bg-primary fixed top-0 left-0 w-full text-center text-[1.3rem] h-full -translate-y-full opacity-0'
+          className='menu bg-primary fixed top-0 left-0 w-full text-center text-[1.3rem] h-full -translate-y-full opacity-0 menu-nonactive'
           style={{ transition: '0.7s cubic-bezier(0.74, -0.03, 0.83, 0.67)' }}
         >
           <div className='flex justify-center flex-col p-1 pt-[5rem] h-full xs:w-80 w-60 m-auto overflow-hidden'>
@@ -153,52 +202,6 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className='nav-container max-w-[1350px] relative grid justify-between items-center my-0 mx-auto'>
-          <Link
-            to={'/'}
-            className='brand text-[1.6rem] text-[#915eff]'
-            onClick={handleLogoClick}
-          >
-            TreezCode
-          </Link>
-          <div className='nav-container-inner my-0 mx-auto w-full max-w-[1000px] flex justify-between items-center'>
-            <ul className='navlinks w-full my-0 mx-2'>
-              {navLinks.map((link) => (
-                <li
-                  key={link.id}
-                  className={`${
-                    activeLink === link.title ? 'scale-110' : ''
-                  } text-[18px] font-medium cursor-pointer inline-block px-2 `}
-                  onClick={() => handleNavbarClick(link)}
-                >
-                  <a href={`#${link.id}`} className='hover:text-secondary'>{link.title}</a>
-                </li>
-              ))}
-            </ul>
-            <form>
-              <div className='input-wrap flex justify-center items-center py-2'>
-                <input type='search' placeholder='Search...' className='px-1' />
-                <button type='submit'>
-                  <FaSearch className='transition duration-300 hover:text-secondary' />
-                </button>
-              </div>
-            </form>
-          </div>
-
-          {!menuOpen ? (
-            <FaBars
-              id='menu-btn'
-              className='text-[1.2rem] cursor-pointer transition duration-300 hover:text-secondary ml-auto'
-              onClick={toggleMenu}
-            />
-          ) : (
-            <FaWindowClose
-              id='menu-btn'
-              className='text-[1.2rem] cursor-pointer transition duration-300 hover:text-secondary ml-auto'
-              onClick={toggleMenu}
-            />
-          )}
-        </div>
       </nav>
     </header>
   );
