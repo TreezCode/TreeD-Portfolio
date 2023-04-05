a9b4e8d67cf8b170828bbfea05347109323f2e54
TreezCode <TreezCode@gmail.com>
2023-04-04 20:46:30 -0400

    refactor: Use rootMargin option instead of threshold for optimized Intersection Observer

--- frontend/src/utils/useActiveSection.js
+++ frontend/src/utils/useActiveSection.js
@@ -1,48 +1,32 @@
 import { useState, useEffect } from 'react';
 
-export const useActiveSection = () => {
-  const [activeSection, setActiveSection] = useState(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setActiveSection(entry.target.dataset.section ?? '');
-        }
-      });
-    }, { threshold: 0.15 }); // set initial threshold to 0
-
-    const sections = document.querySelectorAll('section');
-    sections.forEach((section, i) => {
-      const resizeObserver = new ResizeObserver((entries) => {
-        const { height } = entries[0].contentRect;
-        const prevThreshold = observer.thresholds[0] || 0;
-        const newThreshold = height > window.innerHeight * 0.75 ? 0.25 : height / window.innerHeight;
-        if (newThreshold > prevThreshold) {
-          observer.unobserve(section);
-          observer.observe(section, { threshold: newThreshold });
-        }
-      });
-      resizeObserver.observe(section);
-      const height = section.getBoundingClientRect().height;
-      const threshold = height > window.innerHeight * 0.75 ? 0.25 : height / window.innerHeight;
-      observer.observe(section, { threshold });
-    });
-
-    // Remove the observer as soon as the component is unmounted
-    return () => observer.disconnect();
-  }, []);
-
-  return activeSection;
-};
-
-// 10 line hook for the memes
 // export const useActiveSection = () => {
 //   const [activeSection, setActiveSection] = useState(null);
 //   useEffect(() => {
-//     const observer = new IntersectionObserver(entries => entries.forEach(entry => entry.isIntersecting && setActiveSection(entry.target.dataset.section ?? '')), { threshold: 0.25 });
-//     document.querySelectorAll('section').forEach(section => observer.observe(section));
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           setActiveSection(entry.target.dataset.section ?? '');
+//         }
+//       });
+//     }, { rootMargin: '-50% 0px -50% 0px' }); // set rootMargin to trigger intersection when section is 50% visible
+//     const sections = document.querySelectorAll('section');
+//     sections.forEach((section, i) => {
+//       observer.observe(section);
+//     });
+//     // Remove the observer as soon as the component is unmounted
 //     return () => observer.disconnect();
 //   }, []);
 //   return activeSection;
 // };
+
+// 10 line intersection observer hook for the memes
+export const useActiveSection = () => {
+  const [activeSection, setActiveSection] = useState(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => entries.forEach(entry => entry.isIntersecting && setActiveSection(entry.target.dataset.section ?? '')), { rootMargin: '-50% 0px -50% 0px' });
+    document.querySelectorAll('section').forEach(section => observer.observe(section));
+    return () => observer.disconnect();
+  }, []);
+  return activeSection;
+};
